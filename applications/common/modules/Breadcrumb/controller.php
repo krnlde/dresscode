d41026f46537a9d9a9af20e6cc96694595138ba5
Kai Dorschner <the-kernel32@web.de>
2012-06-11 20:03:13 +0200

    Updated submodules and CSSPie

---
+++ applications/common/modules/Breadcrumb/controller.php
@@ -0,0 +1,31 @@
+<?php
+namespace Dresscode\Controller;
+
+
+/**
+ * @todo howto deal with invisible files?
+ */
+class Breadcrumb extends \Dresscode\Controller
+{
+	public function get(array $params = array())
+	{
+		$path = $this->closest('Root')->getProperty('path');
+		if ($path[0] === '/')
+		{
+			$path = substr($path, 1);
+		}
+		$elements = explode('/', $path);
+		$part = '';
+		foreach ($elements as $element)
+		{
+			$part .= '/'.$element;
+			$file = $this->Application->Model->read($part);
+			$this->node->appendChild($e = $this->dom->createElement('element', $file->getAttribute('alias') ?: $file->getAttribute('name')));
+			$e->setAttribute('path', $this->Application->basePath().$part);
+			if ($element === end($elements))
+			{
+				$e->setAttribute('active', true);
+			}
+		}
+	}
+}
---
+++ applications/common/modules/Breadcrumb/templates/html.xsl
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet
+	version="1.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+	<xsl:template match="breadcrumb">
+		<nav>
+			<ul class="breadcrumb">
+				<xsl:apply-templates select="element"/>
+			</ul>
+		</nav>
+	</xsl:template>
+
+	<xsl:template match="element">
+		<li>
+			<xsl:choose>
+				<xsl:when test="@active">
+					<xsl:attribute name="class">active</xsl:attribute>
+					<xsl:value-of select="."/>
+				</xsl:when>
+				<xsl:otherwise>
+					<a href="{@path}"><xsl:value-of select="."/></a>
+					<span class="divider">/</span>
+				</xsl:otherwise>
+			</xsl:choose>
+		</li>
+	</xsl:template>
+
+</xsl:stylesheet>
