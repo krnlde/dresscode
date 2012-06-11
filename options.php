2be37ac698e6ca6b42ca73815dd8d5dd77731213
Kai Dorschner <the-kernel32@web.de>
2012-06-11 20:08:51 +0200

    Edited options.php

--- options.php
+++ options.php
@@ -8,14 +8,13 @@ $options = array
 		,	'password'	=> ''
 		,	'name'		=> ''
 		)
-	// ,	'name' => '127.0.0.1' // enter a domain here to redirect to another application instead of using $_SERVER['SERVER_NAME'] - the default.
-	,	'name' => 'enfire.me'
-	// ,	'default' => 'enfire.me'
+	,	'name' => '127.0.0.1' // enter a domain here to redirect to another application instead of using $_SERVER['SERVER_NAME'] - the default.
+	// ,	'default' => '127.0.0.1'
 	);
 
 $_GET['debug'] = true; // @debug
 
-/*
+/* @TODO
 
 $options =
 [	'java_path' =>
