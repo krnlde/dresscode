336d32551ff966d39f231cc35e14a9b113d1ada8
Kai Dorschner <the-kernel32@web.de>
2012-07-23 19:35:40 +0200

    Adjusted dimensions for medium size

--- image.php
+++ image.php
@@ -2,9 +2,7 @@
 
 require __DIR__.'/lib/vendor/Opl/src/Opl/Autoloader/GenericLoader.php';
 
-use \Opl\Autoloader\GenericLoader;
-
-$loader = new GenericLoader(__DIR__.'/lib/vendor/');
+$loader = new \Opl\Autoloader\GenericLoader(__DIR__.'/lib/vendor/');
 $loader->addNamespace('Imagine', __DIR__.'/lib/vendor/Imagine/lib');
 $loader->register();
 
@@ -15,15 +13,15 @@ if ($source[0] === '/')
 }
 if (isset($_GET['size']) && $_GET['size'] === 'small')
 {
-	$size		= new Imagine\Image\Box(50, 100);
+	$size = new Imagine\Image\Box(50, 100);
 }
 elseif (isset($_GET['size']) && $_GET['size'] === 'large')
 {
-	$size		= new Imagine\Image\Box(200, 400);
+	$size = new Imagine\Image\Box(200, 400);
 }
 else
 {
-	$size		= new Imagine\Image\Box(100, 200);
+	$size = new Imagine\Image\Box(160, 160);
 }
 // $mode		= Imagine\Image\ImageInterface::THUMBNAIL_INSET;
 $mode		= Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND;
