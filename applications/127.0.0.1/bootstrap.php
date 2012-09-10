74aa27dd53a7a72215b5dc9af01037219241f8fe
Kai Dorschner <the-kernel32@web.de>
2012-09-10 13:30:37 +0200

    Fixed wrong getPath() method.

--- applications/127.0.0.1/bootstrap.php
+++ applications/127.0.0.1/bootstrap.php
@@ -5,5 +5,5 @@ $this->defaultRoute = 'home';
 // initialize DB Connections or something else.
 
 // $xml = new \DomDocument();
-// $xml->load($this->getPath()->getPath().DIRECTORY_SEPARATOR.'translation.xml');
+// $xml->load($this->getPath().DIRECTORY_SEPARATOR.'translation.xml');
 // \Dresscode\Translator::addTranslationsFromXml($xml);
