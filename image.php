8112072b31ec6394a546658b7aca707886e40bc7
Kai Dorschner <the-kernel32@web.de>
2013-11-17 22:00:27 +0100

    Added some caching headers.

--- image.php
+++ image.php
@@ -1,16 +1,15 @@
 <?php
-
 require __DIR__.'/lib/vendor/Opl/src/Opl/Autoloader/GenericLoader.php';
 
 ini_set('memory_limit', '512M');
 
 $loader = new \Opl\Autoloader\GenericLoader(__DIR__.'/lib/vendor/');
+$loader->addNamespace('Dresscode', __DIR__.'/lib/vendor');
 $loader->addNamespace('Imagine', __DIR__.'/lib/vendor/Imagine/lib');
 $loader->register();
 
 $source	= $_GET['source'];
-if ($source[0] === '/')
-{
+if ($source[0] === '/') {
 	$source = $_SERVER['DOCUMENT_ROOT'].$source;
 }
 
@@ -18,14 +17,24 @@ if (!file_exists($source)) {
 	die('Not found');
 }
 
+$mtime = filemtime($source);
 $extension = strtolower(pathinfo($source, PATHINFO_EXTENSION));
 $cachedir = __DIR__.'/cache/';
 $key = md5(serialize($_GET));
 $cachesource = $cachedir.$key.'.'.$extension;
 
+$Request = \Dresscode\Request::getInstance();
+$Response = \Dresscode\Response::getInstance();
+
+if ($Request->if_modified_since && strtotime($mtime) <= strtotime($Request->if_modified_since)) {
+  $Response->end(null, 304); // Not modified
+}
+
+
+header('Date: ' . date('r'));
+header('Last-Modified: ' . date('r', $mtime));
 header('Content-Type: image/'.($extension == 'jpg' ? 'jpeg' : $extension));
-// header('content-disposition....');
-// header('Last-Modified');
+header('Content-Disposition: inline; filename="' . basename($source) . '"');
 
 if (file_exists($cachesource)) {
 	if (filemtime($source) <= filemtime($cachesource)) {
@@ -33,24 +42,19 @@ if (file_exists($cachesource)) {
 	}
 }
 
-if (isset($_GET['width']))
-{
+if (isset($_GET['width'])) {
 	$size = new Imagine\Image\Box( (int)$_GET['width'], (int)(isset($_GET['height']) ? $_GET['height'] : $_GET['width']) );
-}
-else
-{
+} else {
 	$size = new Imagine\Image\Box(709, 709); // max image size in iPad responsive design
 	// $size = new Imagine\Image\Box(370, 370); // max image size in iPad responsive design
 }
 
-if (isset($_GET['crop']) && $_GET['crop'])
-{
+if (isset($_GET['crop']) && $_GET['crop']) {
 	$mode = Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND; // Crop
-}
-else
-{
+} else {
 	$mode = Imagine\Image\ImageInterface::THUMBNAIL_INSET; // Size to fit
 }
+
 $imagine = new \Imagine\Gd\Imagine();
 echo $imagine->open($source)
 	->thumbnail($size, $mode)
